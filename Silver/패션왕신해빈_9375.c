d91ea8ecd230e819d6bcab979d5e5dade436591a
MiniPenguin <wodyd818@naver.com>
2023-11-01 15:43:17 +0900

    2023.11.01

---
+++ Silver/패션왕신해빈_9375.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    /* 종류가 다른 것의 조합의 수 + 총 옷의 개수 */
+
+    char clothes[31][21];
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s", clothes);
+    }
+
+
+
+
+    return 0;
+}
